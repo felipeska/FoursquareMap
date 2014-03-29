b4efdc9439c31ca88c3b7c47642da989e3f6025b
Felipe Calderon Barragan <felipeskabarragan@gmail.com>
2014-03-29 00:08:53 -0500

    use SlidingUpPanelLayout instead fragments :)

--- src/com/codejump/foursquaremap/android/ContentFragment.java
+++ src/com/codejump/foursquaremap/android/ContentFragment.java
@@ -1,14 +1,22 @@
 package com.codejump.foursquaremap.android;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.actionbarsherlock.app.SherlockFragment;
-import com.codejump.foursquaremap.android.R.anim;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelSlideListener;
 
 public class ContentFragment extends SherlockFragment {
+
+	private static final String TAG = ContentFragment.class.getSimpleName();
+	private SlidingUpPanelLayout mUpPanelLayout;
+	private Handler mHandler;
 
 	public ContentFragment() {
 	}
@@ -23,22 +31,54 @@ public class ContentFragment extends SherlockFragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		Log.i(TAG, "created");
+		addMap();
+		mUpPanelLayout = (SlidingUpPanelLayout) getView().findViewById(
+				R.id.sliding_layout);
+		mUpPanelLayout.setShadowDrawable(getResources().getDrawable(
+				R.drawable.above_shadow));
+		mUpPanelLayout.setCoveredFadeColor(getResources().getColor(
+				android.R.color.transparent));
+		mUpPanelLayout.setAnchorPoint(0.04f);
+		mUpPanelLayout.setPanelSlideListener(new PanelSlideListener() {
+			@Override
+			public void onPanelSlide(View panel, float slideOffset) {
+				Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+			}
 
-		getView().findViewById(R.id.header).setOnClickListener(
-				new View.OnClickListener() {
+			@Override
+			public void onPanelExpanded(View panel) {
+				Log.i(TAG, "onPanelExpanded");
 
-					@Override
-					public void onClick(View v) {
-						removeContent();
-					}
-				});
+			}
+
+			@Override
+			public void onPanelCollapsed(View panel) {
+				Log.i(TAG, "onPanelCollapsed");
+
+			}
+
+			@Override
+			public void onPanelAnchored(View panel) {
+				Log.i(TAG, "onPanelAnchored");
+			}
+		});
+
+		mHandler = new Handler();
+		mHandler.postDelayed(new SlidingUpPaneHandler(), 2000);
 	}
 
-	private void removeContent() {
+	private void addMap() {
+		Fragment mFragmentMap = new MapFragment();
 		FragmentManager fragmentManager = getActivity()
 				.getSupportFragmentManager();
-		fragmentManager.beginTransaction()
-				.setCustomAnimations(0, anim.slide_out_to_bottom)
-				.remove(ContentFragment.this).addToBackStack(null).commit();
+		fragmentManager.beginTransaction().replace(R.id.map, mFragmentMap)
+				.commit();
+	}
+
+	class SlidingUpPaneHandler implements Runnable {
+		public void run() {
+			mUpPanelLayout.expandPane(0);
+		}
 	}
 }
--- src/com/codejump/foursquaremap/android/MapActivity.java
+++ src/com/codejump/foursquaremap/android/MapActivity.java
@@ -12,27 +12,19 @@ public class MapActivity extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		addMap();
+		addContent();
 	}
 
 	@Override
 	protected void onResume() {
-		addContent();
 		super.onResume();
 	}
 
-	private void addContent() {
+	void addContent() {
 		Fragment mFragmentContainer = new ContentFragment();
 		FragmentManager fragmentManager = getSupportFragmentManager();
 		fragmentManager.beginTransaction()
 				.setCustomAnimations(anim.slide_in_from_bottom, 0)
 				.replace(R.id.container, mFragmentContainer).commit();
-	}
-
-	private void addMap() {
-		Fragment mFragmentMap = new MapFragment();
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		fragmentManager.beginTransaction().replace(R.id.map, mFragmentMap)
-				.commit();
 	}
 }
